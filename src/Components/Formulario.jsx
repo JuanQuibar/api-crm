0ffc31801f91e59cc2a24e6984c68c5e33e9b2ee
Juan <juanquibar@gmail.com>
2022-05-23 19:55:18 -0300

    se configuro el .env

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -33,7 +33,7 @@ const Formulario = ({cliente, cargando}) => {
 
       if (cliente.id){
         //Editando cliente
-        const url = `http://localhost:4000/clientes/${cliente.id}`
+        const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`
         respuesta = await fetch(url, {
           method: "PUT",
           body: JSON.stringify(valores),
@@ -44,7 +44,7 @@ const Formulario = ({cliente, cargando}) => {
 
       } else {
         //Creando cliente nuevo
-        const url = "http://localhost:4000/clientes"
+        const url = import.meta.env.VITE_API_URL
         respuesta = await fetch(url, {
           method: "POST",
           body: JSON.stringify(valores),
--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -8,7 +8,9 @@ const Inicio = () => {
   useEffect (() => {
     const obtenerClientesApi = async () => {
       try {
-        const url = "http://localhost:4000/clientes"
+        // const url = "https://my-json-server.typicode.com/JuanQuibar/api-crm/clientes"
+        //json-server --watch db.json --port 4000
+        const url = import.meta.env.VITE_API_URL
         const respuesta = await fetch(url)
         const resultado = await respuesta.json()
 
@@ -27,7 +29,7 @@ const Inicio = () => {
     if (confirmar) {
 
       try {
-        const url = `http://localhost:4000/clientes/${id}`
+        const url = `${import.meta.env.VITE_API_URL}/${id}`
         const respuesta = await fetch(url, {
         method: 'DELETE'
       })
